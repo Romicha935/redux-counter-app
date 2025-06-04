d5e43258de792fd922ff899e47020bb9691556a2
romicha-parvin <romichaparvin35@gmail.com>
2025-06-04 12:20:11 +0600

    redux counter app added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Welcome  to react-redux</h1>
+        <Counter/>
+      </div>
+    </>
+  )
+}
+
+export default App
